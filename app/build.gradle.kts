d525c5c7d2400864d268fd76c3c028a5e7d22a3f
Thanapol <Thanapol.Sopanhari@7peakssoftware.com>
2025-08-10 22:02:29 +0700

    build: update setting JvmTarget

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -36,9 +38,13 @@ android {
         sourceCompatibility = JavaVersion.VERSION_18
         targetCompatibility = JavaVersion.VERSION_18
     }
-    kotlinOptions {
-        jvmTarget = "18"
+
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_18
+        }
     }
+
     buildFeatures {
         compose = true
     }

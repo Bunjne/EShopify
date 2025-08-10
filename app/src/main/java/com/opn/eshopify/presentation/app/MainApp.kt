04ac2a4c1157e1e0b3574b9566754ca43fc48909
Thanapol <Thanapol.Sopanhari@7peakssoftware.com>
2025-08-10 22:01:18 +0700

    feat(focusManager): hide keyboard on tapping any outside area

--- app/src/main/java/com/opn/eshopify/presentation/app/MainApp.kt
+++ app/src/main/java/com/opn/eshopify/presentation/app/MainApp.kt
@@ -1,52 +1,46 @@
 package com.opn.eshopify.presentation.app
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
-import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.only
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.semantics.testTagsAsResourceId
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
 import com.opn.eshopify.presentation.navigation.MainNavHost
 
 @Composable
 fun MainApp(
     appState: MainAppState = rememberMainAppState()
 ) {
-    val snackBarHostState = remember { SnackbarHostState() }
+    val localFocusManager = LocalFocusManager.current
 
-    Scaffold(
+    Box(
         modifier = Modifier
-            .semantics { testTagsAsResourceId = true },
-        containerColor = MaterialTheme.colorScheme.background,
-        contentColor = MaterialTheme.colorScheme.onBackground,
-        snackbarHost = { SnackbarHost(snackBarHostState) },
-    ) { padding ->
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(padding)
-                .consumeWindowInsets(padding)
-                .windowInsetsPadding(
-                    WindowInsets.safeDrawing.only(
-                        WindowInsetsSides.Horizontal, // Top and bottom would be handled by each specific screen
-                    ),
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+            .windowInsetsPadding(
+                WindowInsets.safeDrawing.only(
+                    WindowInsetsSides.Horizontal, // Top and bottom would be handled by each specific screen
                 ),
-        ) {
-            MainNavHost(
-                appState = appState,
             )
-        }
+            .pointerInput(Unit) {
+                detectTapGestures(
+                    onTap = {
+                        localFocusManager.clearFocus()
+                    }
+                )
+            },
+    ) {
+        MainNavHost(
+            appState = appState,
+        )
     }
 }

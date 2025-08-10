73d71173f90d6adb23aedefec9820d20cb9d8e4a
Thanapol <Thanapol.Sopanhari@7peakssoftware.com>
2025-08-10 15:20:05 +0700

    chore: add Mockk for mocking and writing tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,10 +65,14 @@ dependencies {
     implementation(libs.koin.compose)
 
     testImplementation(libs.junit)
+    testImplementation(libs.mockk)
+    testImplementation(libs.kotlinx.datetime)
+    testImplementation(libs.kotlinx.coroutines.test)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+    androidTestImplementation(libs.mockk)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
